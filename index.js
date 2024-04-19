dad06e0ab15ddf62351cff77d0632c947b0a890e
dangthangquoc <dangquocthang2004@gmail.com>
2024-04-19 18:32:52 +0700

    update

--- index.js
+++ index.js
@@ -78,3 +78,112 @@ const userImgUpload = multer({ storage: userImgStorage });
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
+
+
+// My Account page
+app.get('/myAccount', requireAuth, checkUser, async (req, res) => {
+  try {
+    // Get the user from res.locals
+    let user = res.locals.user;
+  
+    // If the user is not defined, set it to null and proceed to the next middleware
+    if (!user) {
+      res.locals.user = null;
+      next();
+      return;
+    }
+  
+    // Fetch all authors and publishers from the database
+    const player = await Player.find();
+    const team = await Team.find();
+    const category = await Category.find();
+
+  
+    // Fetch the user's details and populate their deals
+    const userDetails = await User.findById(user._id)
+      .populate('deals')
+      .exec();
+  
+    // If the user details are not found, return a 404 error
+    if (!userDetails) {
+      console.error('User not found:', userDetails._id);
+      return res.status(404).json({ success: false, message: 'User not found' });
+    }
+  
+    // Fetch all transactions
+    const transactions = await Transaction.find();
+  
+    // Update the status and fine of overdue transactions
+    await Promise.all(transactions.map(async (transaction) => {
+      if ((transaction.returnDate < Date.now() && transaction.status == 'Reserved') || (transaction.returnDate < Date.now() && transaction.status == 'Overdue')) {
+        await Transaction.findByIdAndUpdate(
+          transaction._id,
+          {
+            $set: {
+              status: 'Overdue',
+              fine: 1000 * Math.floor((Date.now() - new Date(transaction.returnDate)) / (1000 * 60 * 60 * 24))
+            }
+          },
+          { new: true },
+        );
+      }
+    }));
+  
+    // Fetch the book details for each of the user's active transactions
+    const allActiveTransactions = await Promise.all(
+      userDetails.activeTransactions.map(async (transaction) => {
+        const book = await getBookById(transaction.bookId);
+        return {
+          bookTitle: book.title,
+          status: transaction.status,
+          pickUpDate: transaction.pickUpDate,
+          returnDate: transaction.returnDate,
+          fine: transaction.fine,
+        };
+      })
+    );
+  
+    // Fetch the book details for each of the user's previous transactions
+    const allPrevTransactions = await Promise.all(
+      userDetails.prevTransactions.map(async (transaction) => {
+        const book = await getBookById(transaction.bookId);
+        return {
+          bookTitle: book.title,
+          status: transaction.status,
+          pickUpDate: transaction.pickUpDate,
+          returnDate: transaction.returnDate,
+          fine: transaction.fine,
+        };
+      })
+    );
+  
+    // Fetch the user and book details for each transaction
+    const transactionsWithDetails = await Promise.all(
+      transactions.map(async (transaction) => {
+        const user = await User.findById(transaction.userId).exec();
+        const book = await Book.findById(transaction.bookId).exec();
+  
+        const userEmail = user ? user.email : 'User not found';
+        const bookTitle = book ? book.title : 'Book not found';
+  
+        return {
+          _id: transaction._id,
+          userEmail: userEmail,
+          bookTitle: bookTitle,
+          status: transaction.status,
+          pickUpDate: transaction.pickUpDate,
+          returnDate: transaction.returnDate,
+          fine: transaction.fine,
+        };
+      })
+    );
+  
+    // Render the 'myAccount' page with the fetched data
+    res.render('myAccount', { user: user, books: books, allActiveTransactions, allPrevTransactions, transactions: transactionsWithDetails, authors: authors, publishers: publishers });
+  
+  } catch (error) {
+    // Log any error that occurs and return a 500 error
+    console.error('Error processing transactions:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
